255f3e2ec55bd2063b23bb56f75ccb3caa4cf636
shawnljohnson1980 <78113657+shawnljohnson1980@users.noreply.github.com>
2021-04-07 13:09:35 -0400

    assignment completed

---
+++ oop_1.py
@@ -0,0 +1,23 @@
+class User:
+    def __init__(self, name_input, email_input):
+        self.name=name_input
+        self.email=email_input
+        self.account_balance=0.0
+
+    def make_deposit(self, amount):
+            self.account_balance +=amount
+            return self
+        
+    def venmo (self, amount, payee):
+            self.account_balance -=amount
+            payee.account_balance +=amount
+            return self
+
+        
+shawn = User("Shawn","shawn@mail.com")
+andrew= User ("Andrew","andrew@mail.com")
+
+
+shawn.make_deposit(700).venmo(500, andrew)
+print(f"Shawn's Account balance: ${shawn.account_balance} ")
+print(f"Andrews account balance: ${andrew.account_balance}")
